c5c922ccd2e0f6e7affd71cc0f0c6aecf4a1f261
ajtrao <ajtrao007@gmail.com>
2023-05-19 18:27:32 +0000

    test4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ require "active_support/core_ext/integer/time"
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
