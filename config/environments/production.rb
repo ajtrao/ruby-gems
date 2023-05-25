bf14eb6beecd82b2a205f004aa9b66b857f7eb1d
ajtrao <ajtrao007@gmail.com>
2023-05-25 15:19:25 +0000

    testing 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
   Rails.application.configure do
-    config.action_mailer.default_url_options = {host: "schoolzphs.herokuapp.com/", protocol: "https"}
+    config.action_mailer.default_url_options = {host: "schoolzphs.herokuapp.com", protocol: "https"}
     config.action_mailer.perform_deliveries = true
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
