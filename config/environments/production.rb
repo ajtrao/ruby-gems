ae583ff72c08e970d14bf71c71c7609c0e585ebb
ajtrao <ajtrao007@gmail.com>
2023-05-25 14:16:33 +0000

    Gmail SMTP test in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,18 +3,19 @@ require "active_support/core_ext/integer/time"
   Rails.application.configure do
     config.action_mailer.default_url_options = {host: "schoolzphs.herokuapp.com/", protocol: "https"}
     config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-  
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-    port: 587,
-    address: 'email-smtp.us-east-2.amazonaws.com',
-    user_name: 'AKIAWHPZRY7VF4DQREPK',
-    password: 'BOsPKsi7OWzKddDA2bQ87XBJEMIr/NMz540j/ozGJ9f2',
-    authentication: :plain,
-    enable_starttls_auto: true
-    
-  }
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'db47b22ff7c8456081d36cc20551b966.vfs.cloud9.us-east-1.amazonaws.com',
+    user_name:            Rails.application.credentials.dig(:gmail_smtp, :username),
+    password:             Rails.application.credentials.dig(:gmail_smtp, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
+  
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -80,7 +81,7 @@ require "active_support/core_ext/integer/time"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
